391c70672b0e24ab18d098952f9fa26ff2ef59fa
ferrell-code <automatedcharlesferrell@gmail.com>
2021-06-10 22:06:41 -0400

    add top bar

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGL1Renderer({
+const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 })
 
@@ -16,3 +16,27 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
+const geometry = new THREE.TorusGeometry(10,3,16,100);
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5,5,5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.0075;
+  torus.rotation.z += 0.005;
+
+  renderer.render(scene, camera);
+}
+
+animate();
