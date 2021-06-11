eb660fb542e1ba40911be3d0f63ffe7a5292294b
ferrell-code <automatedcharlesferrell@gmail.com>
2021-06-11 01:25:58 -0400

    finish bar help with window resize

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ import './style.css'
 
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+function animate(time) {
+  time *= 0.001;
+  scene.background = spaceTexture;
+
+  torus_knot.rotation.x += 0.01;
+  torus_knot.rotation.y += 0.0075;
+  torus_knot.rotation.z += 0.005;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -16,10 +32,10 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry(10,3,16,100);
-const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
-const torus = new THREE.Mesh(geometry, material);
-scene.add(torus);
+const knot_geometry = new THREE.TorusKnotGeometry(10,3,16,100);
+const knot_material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+const torus_knot = new THREE.Mesh(knot_geometry, knot_material);
+scene.add(torus_knot);
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5,5,5);
@@ -27,16 +43,18 @@ pointLight.position.set(5,5,5);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper);
 
+const controls = new OrbitControls(camera, renderer.domElement);
 
-function animate() {
-  requestAnimationFrame(animate);
+const spaceTexture = new THREE.TextureLoader().load('./images/space_background.jpg');
+spaceTexture.minFilter = THREE.LinearFilter;
+scene.background = spaceTexture;
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.0075;
-  torus.rotation.z += 0.005;
-
-  renderer.render(scene, camera);
-}
+window.addEventListener('resize', function() {
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
 animate();
