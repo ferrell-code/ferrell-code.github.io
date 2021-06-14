546abebe6e5c5aa7b4b76d093cef425d37b36050
ferrell-code <automatedcharlesferrell@gmail.com>
2021-06-14 02:58:42 -0400

    refactor and fix orbit

--- main.js
+++ main.js
@@ -7,29 +7,12 @@ import mars_text from './images/texture.jpg';
 import sun_image from './images/2k_sun.jpg';
 import space_image from './images/space_background.jpg';
 
-// while loop for scene animation
-function animate(pi) {
-  scene.background = spaceTexture;
-  torus_knot.rotation.x += 0.01;
-  torus_knot.rotation.y += 0.0075;
-  torus_knot.rotation.z += 0.005;
-  pi  += 0.00001;
-  planet.rotation.y += 0.03;
-  planet.position.x += Math.sin( pi * 0.001) * 0.7;
-  planet.position.z += Math.cos( pi * 0.001) * 0.7;
-
-  sun.rotation.y -= 0.005;
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-}
-
 // create knot object
 function init_torus_knot() {
   const knot_geometry = new THREE.TorusKnotGeometry(10,3,16,100);
   const knot_material = new THREE.MeshStandardMaterial({color: 0x85144});
-  const torus_knot = new THREE.Mesh(knot_geometry, knot_material);
+  torus_knot = new THREE.Mesh(knot_geometry, knot_material);
   torus_knot.position.set(30, -20, 0);
-  return torus_knot
 }
 
 // create planet object
@@ -38,9 +21,8 @@ function init_planet() {
   const planet_texture = new THREE.TextureLoader().load(mars_image);
   const normal_texture = new THREE.TextureLoader().load(mars_text);
   const planet_material = new THREE.MeshStandardMaterial({map: planet_texture, normalMap: normal_texture});
-  const planet = new THREE.Mesh(planet_geometry, planet_material);
-  planet.position.set(0, 30, 0);
-  return planet
+  planet = new THREE.Mesh(planet_geometry, planet_material);
+  planet.position.set(20, 30, 0);
 }
 
 // create sun object
@@ -48,42 +30,59 @@ function init_sun() {
   const sun_geo = new THREE.SphereGeometry(10, 32, 32);
   const sun_texture = new THREE.TextureLoader().load(sun_image);
   const sun_material = new THREE.MeshStandardMaterial({map: sun_texture});
-  const sun = new THREE.Mesh(sun_geo, sun_material);
+  sun = new THREE.Mesh(sun_geo, sun_material);
   sun.position.set(30, 30, 0);
-  return sun
 }
 
-// init scene
-const scene = new THREE.Scene();
+// creates light sources
+function init_lights() {
+  pointLight = new THREE.PointLight(0xffffff);
+  pointLight.position.set(5,5,5);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  ambientLight = new THREE.AmbientLight(0xffffff);
+}
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
-})
+// creates camera
+function init_camera() {
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  camera.position.setZ(100);
+}
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(100);
+// creates renderer
+function init_renderer() {
+  renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+  })
 
-renderer.render(scene, camera);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
+// creates background
+function init_background() {
+  spaceTexture = new THREE.TextureLoader().load(space_image);
+  spaceTexture.minFilter = THREE.LinearFilter;
+  scene.background = spaceTexture;
+}
 
-const torus_knot = init_torus_knot();
-const planet = init_planet();
-const sun = init_sun();
+// creates scene
+function init() {
+  scene = new THREE.Scene();
 
-scene.add(torus_knot, planet, sun);
+  init_camera();
+  init_renderer();
+  init_torus_knot();
+  init_sun();
+  init_lights();
+  init_background();
+  init_planet();
 
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5,5,5);
+  scene.add(torus_knot, planet, sun, ambientLight, pointLight);
+}
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
-
-const spaceTexture = new THREE.TextureLoader().load(space_image);
-spaceTexture.minFilter = THREE.LinearFilter;
-scene.background = spaceTexture;
+// main function
+let camera, renderer, torus_knot, planet, sun, pointLight, ambientLight, spaceTexture, scene;
+init();
 
 //make backround stretch with screen as well as objects
 window.addEventListener('resize', function() {
@@ -92,5 +91,26 @@ window.addEventListener('resize', function() {
   renderer.setSize( window.innerWidth, window.innerHeight);
 });
 
-let pi = Math.PI;
-animate(pi);
+//radius and theta for polar cordinates
+var r = 35;
+var theta = 0;
+// How fast theta increments (orbit velocity)
+var dTheta = 2 * Math.PI / 800;
+
+// while loop for scene animation
+var animate = function() {
+  scene.background = spaceTexture;
+  torus_knot.rotation.x += 0.01;
+  torus_knot.rotation.y += 0.0075;
+  torus_knot.rotation.z += 0.005;
+  planet.rotation.y += 0.03;
+
+  theta += dTheta;
+  planet.position.x = r * Math.cos(theta) + 20;
+  planet.position.z = r * Math.sin(theta) + 5;
+  sun.rotation.y -= 0.005;
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+// control loop
+animate();
